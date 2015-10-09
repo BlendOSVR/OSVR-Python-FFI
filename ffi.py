c7efcb985f910fb6c57846485479a9bd1d274cac
awang94 <adam.wang94@gmail.com>
2015-10-09 15:14:29 -0400

    updated ffi.py

--- ffi.py
+++ ffi.py
@@ -1,7 +1,22 @@
-#Author: Adam Wang
 from ctypes import cdll
 
 mylib = cdll.LoadLibrary("osvrClientKit")
+
+def osvrClientInit(applicationIdentifier, flags):
+	rval = mylib.osvrClientInit()
+	return rval
+
+def osvrClientUpdate(ctx):
+	rval = mylib.osvrClientUpdate(ctx)
+	return rval
+
+def osvrClientCheckStatus(ctx):
+	rval = mylib.osvrClientCheckStatus(ctx)
+	return rval
+
+def osvrClientShutdown(ctx):
+	rval = mylib.osvrClientShutdown(ctx)
+	return rval
 
 def osvrClientGetInterface(ctx, path, iface):
 	return mylib.osvrClientGetInterface(ctx, path, iface)
