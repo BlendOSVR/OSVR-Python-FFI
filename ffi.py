4a61e1cd713c39fd3e1f37cb9442edaf6a52743f
Sarah Rust <serust@cs.unc.edu>
2015-10-16 11:50:27 -0500

    Added dev branch. added display api functions

--- ffi.py
+++ ffi.py
@@ -29,3 +29,63 @@ def osvrClientGetStringParameterLength(ctx, path, len):
 	
 def osvrClientGetStringParameter(ctx, path, buf, len):
 	return mylib.osvrClientGetStringParameter(ctx, path, buf, len);
+
+def osvrClientGetDisplay(ctx, disp)
+    return mylib.osvrClientGetDisplay(ctx, disp)
+
+def osvrClientFreeDisplay(disp)
+    return mylib.osvrClientFreeDisplay(disp)
+
+def osvrClientCheckDisplayStartup(disp)
+    return mylib.osvrClientCheckDisplayStartup(disp)
+
+def osvrClientGetNumDisplayInputs(disp, numDisplayInputs)
+    return mylib.osvrClientGetNumDisplayInputs(disp, numDisplayInputs)
+
+def osvrClientGetDisplayDimensions(disp, displayInputIndex, width, height)
+    return mylib.osvrClientGetDisplayDimensions(disp, displayInputIndex, width, height)
+
+def osvrClientGetNumViewers(disp, viewers)
+    return mylib.osvrClientGetNumViewers(disp, viewers)
+
+def osvrClientGetViewerPose(disp, viewer, pose)
+    return mylib.osvrClientGetViewerPose(disp, viewer, pose)
+
+def osvrClientGetNumEyesForViewer(disp, viewer, eyes)
+    return mylib.osvrClientGetNumEyesForViewer(disp, viewer, eyes)
+
+def osvrClientGetViewerEyePose(disp, viewer, eye, pose)
+    return mylib.osvrClientGetViewerPose(disp, viewer, eye, pose)
+
+def osvrClientGetViewerEyeViewMatrixd(disp, viewer, eye, flags, mat)
+    return mylib.osvrClientGetViewerEyeViewMatrixd(disp, viewer, eye, flags, mat)
+
+def osvrClientGetViewerEyeViewMatrixf(disp, viewer, eye, flags, mat)
+    return mylib.osvrClientGetViewerEyeViewMatrixd(disp, viewer, eye, flags, mat)
+
+def osvrClientGetNumSurfacesForViewerEye(disp, viewer, eye, surfaces)
+    return mylib.osvrClientGetNumSurfacesForViewerEye(disp, viewer, eye, surfaces)
+
+def osvrClientGetRelativeViewportForViewerEyeSurface(disp, viewer, eye, surface, left, bottom, width, height)
+    return mylib.osvrClientGetRelativeViewportForViewerEyeSurface(disp, viewer, eye, surface, left, bottom, width, height)
+
+def osvrClientGetViewerEyeSurfaceDisplayInputIndex(disp, viewer, eye, surface, displayInput)
+    return osvrClientGetViewerEyeSurfaceDisplayInputIndex(disp, viewer, eye, surface, displayInput)
+
+def osvrClientGetViewerEyeSurfaceProjectionMatrixd(disp, viewer, eye, surface, near, far, flags, matrix)
+    return mylib.osvrClientGetViewerEyeSurfaceProjectionMatrixd(disp, viewer, eye, surface, near, far, flags, matrix)
+
+def osvrClientGetViewerEyeSurfaceProjectionMatrixf(disp, viewer, eye, surface, near, far, flags, matrix)
+    return mylib.osvrClientGetViewerEyeSurfaceProjectionMatrixf(disp, viewer, eye, surface, near, far, flags, matrix)
+
+def osvrClientGetViewerEyeSurfaceProjectionClippingPlanes(disp, viewer, eye, surface, left, right, bottom, top)
+    return mylib.osvrClientGetViewerEyeSurfaceProjectionClippingPlanes(disp, viewer, eye, surface, left, right, bottom, top)
+
+def osvrClientDoesViewerEyeSurfaceWantDistortion(disp, viewer, eye, surface, distortionRequested)
+    return mylib.osvrClientDoesViewerEyeSurfaceWantDistortion(disp, viewer, eye, surface, distortionRequested)
+
+def osvrClientGetViewerEyeSurfaceRadialDistortionPriority(disp, viewer, eye, surface, priority)
+    return mylib.osvrClientGetViewerEyeSurfaceRadialDistortionPriority(disp, viewer, eye, surface, priority)
+
+def osvrClientGetViewerEyeSurfaceRadialDistortion(disp, viewer, eye, surface, params)
+    return mylib.osvrClientGetViewerEyeSurfaceRadialDistortion(disp, viewer, eye, surface, params)
